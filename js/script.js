820f28cfaa495755055b9bd05b6b145bf3bd7a84
dariomontani <dariomontani@gmail.com>
2021-12-16 16:15:05 +0100

    next prev

--- js/script.js
+++ js/script.js
@@ -2,11 +2,30 @@ const app = new Vue(
     {
         el: '#app',
         data: {
+            n: 0,
 
+            images: [
+                'image1.jpg',
+                'image2.jpg',
+                'image3.jpg',
+                'image4.jpg'
+            ]
         },
 
         methods: {
-            
+            next: function () {
+                this.n += 1;
+                if (this.n == this.images.length) {
+                    this.n = 0;
+                }
+            },
+
+            prev: function() {
+                this.n -= 1;
+                if (this.n < 0) {
+                    this.n = this.images.length - 1;
+                }
+            }
         }
     }
-)
+);
