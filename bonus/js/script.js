795463c7eca4a9a427f65d7ffd9cbca59aaa266d
dariomontani <dariomontani@gmail.com>
2021-12-16 17:05:44 +0100

    bonus 1

--- bonus/js/script.js
+++ bonus/js/script.js
@@ -10,6 +10,7 @@ const app = new Vue(
                 'image3.jpg',
                 'image4.jpg'
             ]
+
         },
 
         methods: {
@@ -25,7 +26,29 @@ const app = new Vue(
                 if (this.n < 0) {
                     this.n = this.images.length - 1;
                 }
-            }
+            },
+
+            onePoint: function () {
+                this.n = 0;
+
+            },
+
+            twoPoint: function () {
+                this.n = 1;
+            },
+
+            threePoint: function () {
+                this.n = 2;
+            },
+
+            fourPoint: function () {
+                this.n = 3;
+            },
         }
     }
 );
+
+// Bonus:
+// 1 - al click su uno dei pallini mostrare l’immagine in posizione corrispondente
+// 2 - applicare l’autoplay allo slider: ogni 3 secondi, cambia immagine automaticamente
+// 3 - quando il mouse va in hover sullo slider, bloccare l’autoplay e farlo riprendere quando esce
